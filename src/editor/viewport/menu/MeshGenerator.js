a1fd2cd5145bb3a711b102a88dff91eea94882e9
LadDeep <deeplad2000@gmail.com>
2022-03-28 19:05:32 +0530

    Refactoring

--- src/editor/viewport/menu/MeshGenerator.js
+++ src/editor/viewport/menu/MeshGenerator.js
@@ -34,93 +34,106 @@ export default class MeshGenerator{
         var geometry, material, properties, mesh;
         material = new THREE.MeshBasicMaterial({color:0x8e9091});
         switch(objectType){
+            
             case OBJECT_TYPE.MESH.PLANE:
                 geometry = new THREE.PlaneGeometry(1, 1);
                 material = new THREE.MeshBasicMaterial({color:0x8e9091 , side: THREE.DoubleSide});
-                break;
-            case OBJECT_TYPE.MESH.CUBE:
-                geometry = new THREE.BoxGeometry(1, 1, 1);
-                break;
-            case OBJECT_TYPE.MESH.CIRCLE:
-                geometry = new THREE.CircleGeometry(1, 10);
-                break;
-            case OBJECT_TYPE.MESH.UVSPHERE:
-                geometry = new THREE.SphereGeometry(1, 30, 30);
-                break;
-            case OBJECT_TYPE.MESH.ICOSPHERE:
-                geometry = new THREE.IcosahedronGeometry(1, 2);
-                break; 
-            case OBJECT_TYPE.MESH.CYLINDER:
-                geometry = new THREE.CylinderGeometry(1, 1, 5, 10, 10);
-                break; 
-            case OBJECT_TYPE.MESH.CONE:
-                geometry = new THREE.ConeGeometry(1, 5, 10, 10);
-                break; 
-            case OBJECT_TYPE.MESH.TORUS:
-                geometry = new THREE.TorusGeometry(10, 5, 10, 30);
-                break; 
-            case OBJECT_TYPE.CAMERA:
-                geometry = new THREE.PerspectiveCamera(75, 1920/1200, 0.1, 1000);
-                break;
-            case OBJECT_TYPE.LIGHT:
-                geometry = new THREE.Light();
-                break; 
-        }
-        geometry.position.set(this.cursorPoint);
-        mesh = new InteractiveMesh(this.viewport, geometry, material);
-        
-        if(attachProperties){
-
-            switch(objectType){
-                
-                case OBJECT_TYPE.MESH.PLANE:
+                geometry.position.set(this.cursorPoint);
+                mesh = new InteractiveMesh(this.viewport, geometry, material);
+                if(attachProperties){
                     properties = new PlaneProperty(mesh);
                     properties.initPlaneProperties();
-                    break;
-    
-                case OBJECT_TYPE.MESH.CUBE:
+                    
+                }
+                mesh.properties = properties;
+                return mesh;
+            
+            case OBJECT_TYPE.MESH.CUBE:
+                geometry = new THREE.BoxGeometry(1, 1, 1);
+                geometry.position.set(this.cursorPoint);
+                mesh = new InteractiveMesh(this.viewport, geometry, material);
+                if(attachProperties){
                     properties = new BoxProperty(mesh);
                     properties.initCubeProperties();
-                    break;
-    
-                case OBJECT_TYPE.MESH.CIRCLE:
+                }
+                mesh.properties = properties;
+                return mesh;
+            
+            case OBJECT_TYPE.MESH.CIRCLE:
+                geometry = new THREE.CircleGeometry(1, 10);
+                geometry.position.set(this.cursorPoint);
+                mesh = new InteractiveMesh(this.viewport, geometry, material);
+                if(attachProperties){
                     properties = new CircleProperty(mesh);
                     properties.initCircleProperties();
-                    break;
-    
-                case OBJECT_TYPE.MESH.UVSPHERE:
+                }
+                mesh.properties = properties;
+                return mesh;
+            
+            case OBJECT_TYPE.MESH.UVSPHERE:
+                geometry = new THREE.SphereGeometry(1, 30, 30);
+                geometry.position.set(this.cursorPoint);
+                mesh = new InteractiveMesh(this.viewport, geometry, material);
+                if(attachProperties){
                     properties = new SphereProperty(mesh);
                     properties.initSphereProperties();
-                    break;
-    
-                case OBJECT_TYPE.MESH.ICOSPHERE:
+                }
+                mesh.properties = properties;
+                return mesh;
+            
+            case OBJECT_TYPE.MESH.ICOSPHERE:
+                geometry = new THREE.IcosahedronGeometry(1, 2);
+                geometry.position.set(this.cursorPoint);
+                mesh = new InteractiveMesh(this.viewport, geometry, material);
+                if(attachProperties){
                     properties = new IcosahedronProperty(mesh);
                     properties.initIcosahedronProperties();
-                    break; 
-    
-                case OBJECT_TYPE.MESH.CYLINDER:
+                }
+                mesh.properties = properties;
+                return mesh; 
+            
+            case OBJECT_TYPE.MESH.CYLINDER:
+                geometry = new THREE.CylinderGeometry(1, 1, 5, 10, 10);
+                if(attachProperties){
                     properties = new CylinderProperty(mesh);
                     properties.initCylinderProperties();
-                    break; 
-                    
-                case OBJECT_TYPE.MESH.CONE:
+                }
+                mesh.properties = properties;
+                return mesh; 
+            
+            case OBJECT_TYPE.MESH.CONE:
+                geometry = new THREE.ConeGeometry(1, 5, 10, 10);
+                geometry.position.set(this.cursorPoint);
+                mesh = new InteractiveMesh(this.viewport, geometry, material);
+                if(attachProperties){
                     properties = new ConeProperty(mesh);
                     properties.initConeProperties();
-                    break; 
-    
-                case OBJECT_TYPE.MESH.TORUS:
+                }
+                mesh.properties = properties;
+                return mesh; 
+            
+            case OBJECT_TYPE.MESH.TORUS:
+                geometry = new THREE.TorusGeometry(10, 5, 10, 30);
+                geometry.position.set(this.cursorPoint);
+                mesh = new InteractiveMesh(this.viewport, geometry, material);
+                if(attachProperties){
                     properties = new TorusProperty(mesh);
                     properties.iniTorusProperties();
-                    break; 
-    
-                case OBJECT_TYPE.CAMERA:
-                    break;
-    
-                case OBJECT_TYPE.LIGHT:
-                    break; 
-            }
-            mesh.properties = properties;
+                }
+                mesh.properties = properties;
+                return mesh; 
+            
+            case OBJECT_TYPE.CAMERA:
+                geometry = new THREE.PerspectiveCamera(75, 1920/1200, 0.1, 1000);
+                geometry.position.set(this.cursorPoint);
+                mesh = new InteractiveMesh(this.viewport, geometry, material);
+                return mesh;
+            
+            case OBJECT_TYPE.LIGHT:
+                geometry = new THREE.Light();
+                geometry.position.set(this.cursorPoint);
+                mesh = new InteractiveMesh(this.viewport, geometry, material);
+                return mesh; 
         }
-        return mesh; 
     }
 }
